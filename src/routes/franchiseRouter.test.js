bcdb760516ef5ed0c027f4758cf4c8ac061931aa
Merica Rowley <mericarowley@gmail.com>
2025-09-27 00:01:57 -0600

    wrote some more franchise tests; can't get the delete franchise test quite right yet

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -34,26 +34,53 @@ async function createAdminUser() {
 //   return { ...user, password: "supersecretpw" };
 // }
 
-test("get franchises", async () => {
-  const franchiseRes = await request(app).get("/api/franchise");
-  expect(franchiseRes.status).toBe(200);
-  expect(Array.isArray(franchiseRes.body.franchises)).toBe(true);
-  expect(franchiseRes.body.more).toBeDefined();
-});
+test("get franchises", getFranchises);
 
-test("create franchise as admin", async () => {
+async function getFranchises() {
+  let allFranchises = [];
+  let page = 0;
+  let more = true;
+
+  while (more) {
+    const nextPageRes = await request(app).get(`/api/franchise?page=${page}`);
+    expect(nextPageRes.status).toBe(200);
+    expect(Array.isArray(nextPageRes.body.franchises)).toBe(true);
+    expect(nextPageRes.body.more).toBeDefined();
+
+    allFranchises = allFranchises.concat(nextPageRes.body.franchises);
+    more = nextPageRes.body.more;
+    page++;
+  }
+
+  return allFranchises;
+}
+
+test("create franchise as admin", adminCreateFranchise);
+
+async function adminCreateFranchise() {
   const adminUser = await createAdminUser();
   const loginRes = await request(app).put("/api/auth").send(adminUser);
   expectValidJwt(loginRes.body.token);
 
-  const newFranchise = { name: adminUser.name, admins: [adminUser] };
+  const franchisesBefore = await getFranchises();
+
+  const newFranchise = { name: randomName(), admins: [adminUser] };
   const createFranchiseRes = await request(app)
     .post("/api/franchise")
     .set("Authorization", `Bearer ${loginRes.body.token}`)
     .send(newFranchise);
   expect(createFranchiseRes.status).toBe(200);
   expect(createFranchiseRes.body).toMatchObject(newFranchise);
-});
+
+  const franchisesAfter = await getFranchises();
+  expect(franchisesAfter.length).toBe(franchisesBefore.length + 1);
+
+  return {
+    franchise: newFranchise,
+    adminUser: adminUser,
+    adminAuthToken: loginRes.body.token,
+  };
+}
 
 test("create franchise as non-admin", async () => {
   const loginRes = await request(app).put("/api/auth").send(testUser);
@@ -68,6 +95,55 @@ test("create franchise as non-admin", async () => {
   expect(createFranchiseRes.status).toBe(403);
   expect(createFranchiseRes.body.message).toBe("unable to create a franchise");
 });
+
+test("get user franchises", getUserFranchises);
+
+async function getUserFranchises() {
+  const { franchise, adminUser, adminAuthToken } = await adminCreateFranchise();
+
+  const getUserFranchisesRes = await request(app)
+    .get(`/api/franchise/${adminUser.id}`)
+    .set("Authorization", `Bearer ${adminAuthToken}`);
+  expect(getUserFranchisesRes.status).toBe(200);
+  expect(Array.isArray(getUserFranchisesRes.body)).toBe(true);
+  expect(getUserFranchisesRes.body).toContainEqual(
+    expect.objectContaining({ name: franchise.name })
+  );
+
+  return {
+    franchise,
+    adminUser,
+    adminAuthToken,
+    franchises: getUserFranchisesRes.body,
+  };
+}
+
+// test("delete franchise", async () => {
+//   const { franchise, adminUser, adminAuthToken, franchises } =
+//     await getUserFranchises();
+
+//   // franchises should contain info for one franchise
+//   const franchiseIDToDelete = franchises[0].id;
+//   expect(franchiseIDToDelete).toBeDefined();
+
+//   // delete the franchise
+//   const franchiseDeleteRes = await request(app)
+//     .delete(`/api/franchise/${franchiseIDToDelete}`)
+//     .set("Authorization", `Bearer ${adminAuthToken}`);
+//   expect(franchiseDeleteRes.status).toBe(200);
+//   expect(franchiseDeleteRes.body).toMatchObject({
+//     message: "franchise deleted",
+//   });
+
+//   const getUserFranchisesRes = await request(app)
+//     .get(`/api/franchise/${adminUser.id}`)
+//     .set("Authorization", `Bearer ${adminAuthToken}`);
+//   expect(getUserFranchisesRes.status).toBe(200);
+//   expect(Array.isArray(getUserFranchisesRes.body)).toBe(true);
+//   expect(getUserFranchisesRes.body).not.toContainEqual(
+//     expect.objectContaining({ name: franchise.name })
+//   );
+// });
 
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(
